19db055fef9b73a373edc3aaca16cd8567620264
Eugene <panteleymonchuk@gmail.com>
2021-03-19 22:40:55 +0300

    [SECTION 3]. Lesson 39. Done HW

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
-
+const routes = require('./routes');
 // Method which actually returns a server. But we need to run this server.
-const server = http.createServer();
+const server = http.createServer(routes);
 
 server.listen(3000);
--- routes.js
+++ routes.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 
+
 const routes = (req, res) => {
   // req.url - '/', всё, что после localhost:3000
   // req.method - 'GET', метод с помощью которого был отправлен запрос
@@ -8,38 +9,89 @@ const routes = (req, res) => {
   // console.log(req.url, req.method, req.headers);
   
   
-  
-  
   if (req.url === '/') {
     res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head>');
-    res.write('<title>My first page</title>');
-    res.write('</head>');
-    res.write('<body>');
-    res.write('<form action="/form" method="POST"><input name="message" /><input name="text" /><button type="submit">Submit</button></form>');
-    res.write('</body>');
-    res.write('</html>');
+    res.write(`
+      <html>
+      <head>
+        <title>My first page</title>
+      </head>
+        <body>
+          <form action="/create-user" method="POST">
+            <input name="username" />
+            <button type="submit">Submit</button>
+          </form>
+          <a href="/users">List of Users</a>
+        </body>
+      </html>
+    `);
+    // fs.readFile('./nameOfFile.txt', 'utf8', (err, data) => {
+    //   console.log(data.split('\n'));
+    // });
     return res.end(); // даём понять, что мы закончили ответ
   }
   
-  if (req.url === '/form' && req.method === 'POST') {
+  if (req.url === '/create-user' && req.method === 'POST') {
     const body = [];
-    console.log('Код будет выполнен первым');
-    req.on("data", (chunk) => {
-      body.push(chunk);
-      console.log(chunk);
+    req.on('data', (ch) => {
+      body.push(ch);
     });
-    return req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      console.log(' --- parsedBody --- ', parsedBody);
-      fs.writeFile('./nameOfFile.txt', 'fasdf',(err) => {
-        console.log('код будет выполнен третьим');
-        return res.end();
-      });
+    return req.on('end', () => {
+      fs.readFile('./nameOfFile.txt', (err, content) => {
+        const [, parsedName] = Buffer.concat(body).toString().split('=');
+        // console.log(content.toString());
+        // return res.end();
+        const newContent = [
+          ...content.toString().split('\n').filter(el => el),
+          parsedName
+        ].join('\n');
+        return fs.writeFile('./nameOfFile.txt', newContent, () => {
+          res.statusCode = 302;
+          res.setHeader('Location', '/');
+          return res.end();
+        });
+      })
+    });
+  }
+  
+  if (req.url === '/users') {
+    return fs.readFile('./nameOfFile.txt', (err, contentBuff) => {
+      const namesArr = contentBuff
+        .toString()
+        .split('\n')
+        .filter(el => el)
+        .map(el => `<p>${el}</p>`)
+        .join('');
       
+      res.write(`
+        <html>
+        <head>
+          <title>My first page</title>
+        </head>
+          <body>
+            ${namesArr}
+            <a href="/">Form</a>
+          </body>
+        </html>
+      `);
+      return res.end();
     });
-    console.log('код будет выполнен вторым');
+    // const body = [];
+    // console.log('Код будет выполнен первым');
+    // req.on("data", (chunk) => {
+    //   body.push(chunk);
+    //   console.log(chunk);
+    // });
+    // return req.on("end", () => {
+    //   const parsedBody = Buffer.concat(body).toString();
+    //   console.log(' --- parsedBody --- ', parsedBody);
+    //   fs.writeFile('./nameOfFile.txt', 'fasdf', (err) => {
+    //     console.log('код будет выполнен третьим');
+    //     return res.end();
+    //   });
+    //
+    // });
+    // console.log('код будет выполнен вторым');
   }
   
   console.log('код будет выполнен если в if нету return')
