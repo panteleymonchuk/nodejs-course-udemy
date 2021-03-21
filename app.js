7b32f845590492639e27ba40f5632d5d0f0c71dd
Eugene <panteleymonchuk@gmail.com>
2021-03-21 17:34:11 +0300

    [SECTION 3]. Lesson 65. Work with post requests.

--- app.js
+++ app.js
@@ -1,12 +1,52 @@
 const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const routes = require('./routes');
 
-app.use((req, res, next) => {
-  console.log(req);
-  
+const ROUTES = {
+  root: '/',
+  addProduct: '/add-product',
+  addProductFormdata: '/add-product-formdata'
+};
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(ROUTES.root, (req, res, next) => {
+  console.log('this always runs');
+  next();
+  // res.send('<h1></h1>');
+});
+
+
+app.use(ROUTES.addProduct, (req, res, next) => {
+  res.send(`
+  <html>
+    <head>
+      <title>My first page</title>
+    </head>
+    <body>
+      <form action="${ROUTES.addProductFormdata}" method="POST">
+        <input name="username" />
+        <button type="submit">Submit</button>
+      </form>
+      <a href="/users">List of Users</a>
+    </body>
+  </html>`);
+});
+
+
+
+app.post(ROUTES.addProductFormdata, (req, res, next) => {
+  // console.log(req);
+  console.log(res.body);
+  // res.send('<h1>Test</h1>');
+  res.redirect(ROUTES.root);
+});
+
+
+app.use(ROUTES.root, (req, res, next) => {
   res.send('<h1>Test</h1>');
 });
 
