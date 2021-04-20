a9a19449dbe64986a61308f18a8929fe7a6f6bd1
Eugene <panteleymonchuk@gmail.com>
2021-04-21 00:43:02 +0300

    [SECTION 11]. Lesson 161. Create User model and resync MySQL.

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ const bodyParser = require('body-parser');
 const errorController = require('./controllers/errors');
 const sequelize = require('./utils/database');
 
+
+const Product = require('./models/product');
+const User = require('./models/user');
+
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -22,7 +29,12 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000, null, () => {
-    console.log(`Host started at port 3000`);
-});
+sequelize
+  .sync({ force: true })
+  .then(() => {
+      app.listen(3000, null, () => {
+          console.log(`Host started at port 3000`);
+      });
+  });
+
 
---
+++ models/user.js
@@ -0,0 +1,20 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../utils/database');
+
+
+class User extends Model {
+}
+
+User.init({
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  name: DataTypes.STRING,
+  email: DataTypes.STRING,
+}, { sequelize, modelName: 'user' });
+
+
+module.exports = User;
