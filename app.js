13badea7c1b9d3f86cb5963855e5c59613811dd0
Eugene <panteleymonchuk@gmail.com>
2021-05-27 19:23:12 +0300

    [SECTION 13]. Lesson 210. Add mongoose.

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
+// const mongoConnect = require('./util/database').mongoConnect;
 const User = require('./models/user');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -18,20 +19,25 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  User.findById('60979e8262a6c7918fd9f511')
-    .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById('60979e8262a6c7918fd9f511')
+//     .then(user => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch(err => console.log(err));
+// });
 
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
+// app.use('/admin', adminRoutes);
+// app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(3000);
-});
+mongoose.connect('mongodb+srv://root:rootroot@cluster0.kjmws.mongodb.net/shop', {useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch((err) => console.log(err));
+// mongoConnect(() => {
+//
+// });
